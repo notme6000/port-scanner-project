cf3dd906a272949904f67bc6267dfe8d477ce9c8
notme6000 <sureshjithu9400@gmail.com>
2025-09-20 23:57:53 +0530

    some terminal ui stuff, no logic added for main ports

--- main.c
+++ main.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <string.h>
-//#include <stdlib.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+struct pseudo_header {
+	u_int32_t source_address;
+	u_int32_t dest_address;
+	u_int8_t placeholder;
+	u_int8_t protocol;
+	u_int16_t tcp_length;
+};
 
 typedef struct {
 	char ip[60];
 	char ports[30];
 } IpPorts;
+
 // function declaration
 
 void scan();
